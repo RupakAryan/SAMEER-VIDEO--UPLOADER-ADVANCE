c696b7eca271df35a6f28da9c91087206a8c55cb
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-08-21 23:13:55 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,7 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
+# Don't Remove Credit Tg
+# Ask Doubt on telegrag
 from os import environ
 
-API_ID = int(environ.get("API_ID", "21437108")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "53b13c686a0cce30a8aa35710ac11a62") #Replace with your api hash
+API_ID = int(environ.get("API_ID", "")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
